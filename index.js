e5f2383c03d3ae9fdc628739972d2541785f21c2
Splash-58 <splashhomepainters@gmail.com>
2025-06-25 12:05:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SplashHomePainters from "./SplashHomePainters";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <SplashHomePainters />
+  </React.StrictMode>
+);
